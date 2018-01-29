a29754d9a4a8e950dcdb73684868066eece7a282
Mat Bagnall <matbagnall@gmail.com>
2018-01-28 22:28:47 -0800

    Committing lastest updates

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,6 +3,7 @@ class AppMaker {
   constructor () {
     this.server = 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages';
     this.data;
+    this.friends = [];
   }
   init() {
     this.fetch();
@@ -12,7 +13,7 @@ class AppMaker {
       // This is the url you should use to communicate with the parse API server.
       url: this.server,
       type: 'POST',
-      data: JSON.stringify(message),
+      data: JSON.stringify(message),  
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Message sent');
@@ -51,31 +52,49 @@ class AppMaker {
   }
   renderMessage(obj) {
     var room = app.currentRoom;
-    var messages = obj.results;
-    $('.chat').remove();
-    for (var i = 0; i < messages.length; i++) {
-      if (messages[i].roomname === room) { 
-        var user = _.escape(messages[i].username);
-        var text = _.escape(messages[i].text);
-        $('.chatBox').append('<div class="chat"><a href=# class="username">' + user + '</a><br><span>' + text + '</span></div>');
-      }    
-    } 
-  }
-  renderRoom(obj) {
-    var messages = obj.results;
-    var rooms = [];
-    $('.room').remove();
-    for (var i = 0; i < messages.length; i++) {
-      var room = _.escape(messages[i].roomname);
-      if (!rooms.includes(room)) {
-        rooms.push(room);
-        $('#roomNav').append('<div class="room"><a href=# class="roomLink">' + room + '</a></div>');
+    if (obj.results) {
+      var messages = obj.results;
+      $('.chat').remove();
+      for (var i = 0; i < messages.length; i++) {
+        if (messages[i].roomname === room) { 
+          var user = _.escape(messages[i].username);
+          var text = _.escape(messages[i].text);
+          $('#chats').append('<div class="chat"><a href=# class="username">' + user + '</a><br><span>' + text + '</span></div>');
+        }    
+      }
+    } else {
+      if (obj.roomname === room) {
+        var user = _.escape(obj.username);
+        var text = _.escape(obj.text);
+        $('#chats').append('<div class="chat"><a href=# class="username">' + user + '</a><br><span>' + text + '</span></div>');
       }
     }
   }
+  renderRoom(obj) {
+    var rooms = [];
+    if (obj.results) {
+      var messages = obj.results;
+      $('.room').remove();
+      for (var i = 0; i < messages.length; i++) {
+        var room = _.escape(messages[i].roomname);
+        if (!rooms.includes(room)) {
+          rooms.push(room);
+          $('#roomNav').append('<div class="room"><a href=# class="roomLink">' + room + '</a></div>');
+        }
+      }
+    } else if (!rooms.includes(obj.roomname)) {
+      rooms.push(obj.roomname);
+      var room = _.escape(obj.roomname);
+      $('#roomNav').append('<div class="room"><a href=# class="roomLink">' + room + '</a></div>');
+    }
+  }
 
-  handleUsernameClick() {
-    
+  handleUsernameClick(username) {
+    if (!this.friends.includes(username)) {
+      this.friends.push(username);
+    } else {
+      this.friends(username, 1);
+    }
   }
   
 }
@@ -84,20 +103,28 @@ let app = new AppMaker();
 
 $(document).ready(function() {
   app.init();
-  app.username = "lil prince";
-  app.currentRoom = "ghetto";
+  app.username = 'lil prince';
+  app.currentRoom = 'lobby';
   
   
-  $('.roomBtn').on('click', function() {
+  $('.roomBtn').on('click', function(e) {
+    e.preventDefault();
+    $('.room').fadeToggle();
+  });
+
+  $('.friendsBtn').on('click', function(e) {
+    e.preventDefault();
     $('.room').fadeToggle();
   });
   
-  $('#roomNav').on('click', '.roomLink', function() {
+  $('#roomNav').on('click', '.roomLink', function(e) {
+    e.preventDefault();
     app.currentRoom = $(this).text();
     app.fetch();
   });
   
-  $('.submitBtn').on('click', function() {
+  $('.submitBtn').on('click', function(e) {
+    e.preventDefault();
     
     var newMessage = {};
     newMessage.username = app.username;
@@ -110,29 +137,51 @@ $(document).ready(function() {
     app.fetch();
   });
   
-  $('.usernameBtn').on('click', function() {
-    app.username = $('.usernameInput').val();
-    $('.usernameInput').val('');
+  $('.usernameBtn').on('click', function(e) {
+    e.preventDefault();
+    app.username = $('.userInput').val();
+    $('.userInput').val('');
     console.log(app.username);
   });
   
-  $('.username').on('click', function() {
-    addClass('friend');
-  });
-  
-  $('.createRoom').on('click', function() {
+  $('.createRoom').on('click', function(e) {
+    e.preventDefault();
     if ($('#roomNav').find('.roomInputCont').length === 0) {
-      $('#roomNav').append('<div class="roomInputCont"><input class="roomInput" type="text" placeholder="room name?"><br><span class="addRoom"><a href="#">Submit</a></span></div>'); 
+      $('#roomNav').append('<section class="roomInputCont"><div><input class="roomInput" type="text" placeholder="room name?"></div><div class="addRoom"><a href="#">Submit</a></div></div>'); 
     } else {
       $('.roomInputCont').remove();
     }
   });
-  
-  $('#roomNav').on('click', '.addRoom', function() {
+  $('.usernameChange').on('click', function(e) {
+    e.preventDefault();
+    if ($('#roomNav').find('.setUser').length === 0) {
+      $('#roomNav').append('<section class="setUser"><div><input class="userInput" type="text" placeholder="user name?"></div><div class="usernameBtn"><a href=#>Submit</a></div></section>'); 
+    } else {
+      $('.setUser').remove();
+    }
+  });
+  $('#roomNav').on('click', '.usernameBtn', function(e) {
+    e.preventDefault();
+    var name = $('.userInput').val();
+    app.username = name;
+    $('.showUserN').html(name);
+    $('.setUser').remove();
+  });
+
+  $('#roomNav').on('click', '.addRoom', function(e) {
+    e.preventDefault();
     var room = $('.roomInput').val();
     $('#roomNav').append('<div class="room"><a href=# class="roomLink">' + room + '</a></div>');
     $('.roomInputCont').remove();
-    
+  });
+
+  $('.username').on('click', function(e) {
+    e.preventDefault();
+    if (!$(this).hasClass('friend')) {
+      $(this).addClass('friend');
+    } else {
+      $(this).removeClass('friend');
+    }
   });
 });
 
--- spec/chatterboxSpec.js
+++ spec/chatterboxSpec.js
@@ -89,7 +89,7 @@ describe('chatterbox', function() {
       it('should be able to add rooms to the DOM', function() {
         app.renderRoom('superLobby');
 
-        expect($('#roomSelect').children().length).to.equal(1);
+        expect($('#roomSelect').children().length).to.equal(6);
       });
 
     });
